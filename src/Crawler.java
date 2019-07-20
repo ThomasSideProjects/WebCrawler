6e6b4d4eee1b6473b430cf896bab4ff2f34b933f
Thomas <908356@swansea.ac.uk>
2019-07-20 21:39:41 +0100

    Added web crawling functionality

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,36 +1,57 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashSet;
-
 
 public class Crawler {
 
-    private int crawlLimit;
+    private final int CRAWL_LIMIT;
+    private int pagesCrawled;
     private HashSet<String> discoveredURLs;
 
-
     public Crawler(String seedURL, int crawlLimit) {
-        this.crawlLimit = crawlLimit;
-        this.discoveredURLs = new HashSet<>();
-        this.discoveredURLs.add(seedURL);
+        CRAWL_LIMIT = crawlLimit;
+        discoveredURLs = new HashSet<>();
+        pagesCrawled = 0;
+        scrapeSite(seedURL);
     }
 
+    private void scrapeSite(String url) {
+        if (pagesCrawled < CRAWL_LIMIT && !discoveredURLs.contains(url)) {
+            try {
 
-    public void scrapeSite(String url) {
-        try {
-            Document document = Jsoup.connect(url).get();
-            String title = document.title();
-            String description = document.select("meta[name=description]").get(0).attr("content");
-            System.out.println(title + " " + description);
-        } catch (IOException e) {
-            e.printStackTrace();
+                Document document = Jsoup.connect(url).get();
+                String title = document.title();
+
+                String description;
+                if (document.select("meta[name=description]").size() == 0)  {
+                    description = "No description for this site";
+                } else {
+                    description = document.select("meta[name=description]").get(0).attr("content");
+                }
+                System.out.println(title + " " + description);
+                System.out.println(url + "\n");
+
+                discoveredURLs.add(url);
+                pagesCrawled++;
+                Elements scrapedLinks = document.select("a[href]");
+
+                for (int i=0; i < scrapedLinks.size(); i++) {
+                    scrapeSite(scrapedLinks.get(i).attr("abs:href"));
+                }
+
+            //For handling malformed urls scraped from hyperlinks
+            } catch (MalformedURLException e) {
+                System.err.println("Unparsable URL found: " + url + ", it has been skipped" + "\n");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
-    }
 
-    public HashSet<String> getDiscoveredURLs() {
-        return this.discoveredURLs;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Crawler crawler = new Crawler("https://www.stackoverflow.com",2);
-        crawler.scrapeSite("https://www.stackoverflow.com");
+        Crawler crawler = new Crawler("https://www.stackoverflow.com",10);
     }
 }
